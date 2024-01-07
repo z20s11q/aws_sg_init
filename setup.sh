eb1b05a00c6a73b3a7be6d05ed163c59fdd8ec2c
z20s11q <1102437769@qq.com>
2024-01-07 14:53:06 +0800

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# 确保脚本以root权限运行
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# 1. 更改root账号密码
+echo 'root:20020705.zsq' | chpasswd
+
+# 2. 替换/etc/ssh/sshd_config配置文件
+# 确保当前路径下有sshd_config文件
+if [ -f "sshd_config" ]; then
+    cp -f sshd_config /etc/ssh/sshd_config
+else
+    echo "sshd_config file does not exist in the current directory."
+    exit 1
+fi
+
+# 3. 执行install-release.sh脚本
+# 确保当前路径下有install-release.sh文件
+if [ -f "install-release.sh" ]; then
+    chmod +x install-release.sh
+    ./install-release.sh
+else
+    echo "install-release.sh file does not exist in the current directory."
+    exit 1
+fi
+
+# 4. 复制config.json到/usr/local/etc/v2ray/
+# 确保当前路径下有config.json文件
+if [ -f "config.json" ]; then
+    mkdir -p /usr/local/etc/v2ray
+    cp -f config.json /usr/local/etc/v2ray/
+else
+    echo "config.json file does not exist in the current directory."
+    exit 1
+fi
+
+# 5. 重启系统
+shutdown -r now
+
+exit 0
